47ba6d43a42c2ad49dfc8b1e82d7772e9f9a9415
kIwwwww1 <danil29070@gmail.com>
2026-04-24 13:03:19 +0300

    up docker compose backend

--- src/backend/dependency.py
+++ src/backend/dependency.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Session
 from typing import Annotated
 from fastapi import Depends
+# 
 from models.base import get_session
 
 SessionDep = Annotated[Session, Depends(get_session)]
