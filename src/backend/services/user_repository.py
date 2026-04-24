010ec3cf245022a616f7fc97e4c1a68392e6e164
kIwwwww1 <danil29070@gmail.com>
2026-04-25 02:48:21 +0300

    Users

--- src/backend/services/user_repository.py
+++ src/backend/services/user_repository.py
@@ -1,10 +1,19 @@
 import asyncio
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.backend.models.user import Users
+from src.backend.schemas.user import UserData
+
 class UserRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    # async def create_user()
+    async def create_user(self, data: UserData) -> str:
+        Users(
+            
+        )
+        await self.session.commit()
+        return '...'
     
     async def get_by_id(self, user_id: int) -> str:# Объект пользователя
         await asyncio.sleep(0.5)
--- src/backend/services/user_service.py
+++ src/backend/services/user_service.py
@@ -1,9 +1,13 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.backend.services.user_repository import UserRepository
+from src.backend.schemas.user import UserData
 
 class UserService():
     def __init__(self, session: AsyncSession) -> None:
         self.db = UserRepository(session)
+
+    async def create_user(self, data: UserData) -> str:
+        ...
 
     async def get_user(self, id: int) -> str: # -> Объект пользователя при работе с бд
         # Тестовый пример без обращения к базе данных
