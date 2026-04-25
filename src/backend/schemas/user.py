a8267756afd67f6330f0baaf61a104dbca8b8cf1
kIwwwww1 <danil29070@gmail.com>
2026-04-25 18:38:46 +0300

    create func generate_unique_id

--- src/backend/schemas/user.py
+++ src/backend/schemas/user.py
@@ -1,4 +1,22 @@
+from random import sample
+from string import ascii_lowercase, ascii_uppercase, digits
 from pydantic import BaseModel, Field, EmailStr
+
+_LOWERCASE = ascii_lowercase
+_UPPERCASE = ascii_uppercase
+_DIGITS = digits
+
+def generate_unique_id(
+        use_lower: int = 2,
+        use_upper: int = 2,
+        use_digits: int = 2,
+    ) -> str:
+    random_lower = sample(_LOWERCASE, use_lower)
+    random_upper = sample(_UPPERCASE, use_upper)
+    random_digit = sample(_DIGITS, use_digits)
+    combined = random_lower + random_upper + random_digit
+    
+    return ''.join(sample(combined, len(combined)))
 
 class UserData(BaseModel):
     user_name: str = Field(min_length=3, max_length=15)
@@ -6,6 +24,6 @@ class UserData(BaseModel):
     password: str = Field(min_length=8, max_length=25)
 
 class UserResponse(UserData):
-    unique_id: str = Field(default_factory=...)
+    unique_id: str = Field(default_factory=lambda: )
     
 
