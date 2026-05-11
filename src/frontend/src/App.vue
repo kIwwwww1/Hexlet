37e7e4e342ce27816d0d0de3ddccabbd8a469e4c
kIwwwww1 <danil29070@gmail.com>
2026-05-11 13:28:59 +0300

    create test button

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const message = ref('Привет, Vue 3!')
+</script>
+
+<template>
+  <div class="app-container">
+    <h1>{{ message }}</h1>
+    <MyComponent />
+    <button class="info-button">
+            <img src="@/assets/images/question-solid.png" alt="info">
+    </button>
+</div>
+</template>
+
+<style scoped>
+.app-container {
+  font-family: sans-serif;
+  text-align: center;
+}
+</style>
